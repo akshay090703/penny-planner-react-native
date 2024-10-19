46a94a601f9b1d6685d06bab29dbf0c2683145da
akshay090703 <3551akshay@gmail.com>
2024-10-20 00:00:03 +0530

    Used Kinde for authentication

---
+++ app/login/index.jsx
@@ -0,0 +1,104 @@
+import { View, Text, Image, StyleSheet } from "react-native";
+import React from "react";
+import { Colors } from "../../constants/Colors";
+import { TouchableOpacity } from "react-native";
+import { client } from "../../utils/kindeConfig";
+import services from "../../utils/services";
+import { useRouter } from "expo-router";
+
+export default function LoginScreen() {
+  const router = useRouter();
+
+  const handleSignIn = async () => {
+    const token = await client.login();
+    if (token) {
+      // User was authenticated
+
+      await services.storeData("login", "true");
+      router.replace("/");
+    }
+  };
+  return (
+    <View
+      style={{
+        display: "flex",
+        alignItems: "center",
+      }}
+    >
+      <Image
+        source={require("../../assets/images/homepage.png")}
+        style={styles.bgImage}
+      />
+      <View
+        style={{
+          backgroundColor: Colors.PRIMARY,
+          width: "100%",
+          height: "100%",
+          padding: 20,
+          marginTop: -30,
+          borderTopLeftRadius: 30,
+          borderTopRightRadius: 30,
+        }}
+      >
+        <Text
+          style={{
+            fontSize: 32,
+            fontWeight: "bold",
+            textAlign: "center",
+            color: Colors.WHITE,
+          }}
+        >
+          Personal Budget Planner
+        </Text>
+
+        <Text
+          style={{
+            fontSize: 17,
+            textAlign: "center",
+            color: Colors.WHITE,
+            marginTop: 20,
+          }}
+        >
+          Stay on Track, Event by Event: Your Personal Budget Planner App!
+        </Text>
+        <TouchableOpacity style={styles.button} onPress={handleSignIn}>
+          <Text
+            style={{
+              textAlign: "center",
+              color: Colors.PRIMARY,
+            }}
+          >
+            Login/Signup
+          </Text>
+        </TouchableOpacity>
+        <Text
+          style={{
+            fontSize: 13,
+            color: Colors.GRAY,
+            marginTop: 10,
+          }}
+        >
+          * By login/signup you will agree to our terms and conditions.
+        </Text>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  bgImage: {
+    width: 200,
+    height: 400,
+    marginTop: 60,
+    borderWidth: 5,
+    borderRadius: 20,
+    borderColor: Colors.BLACK,
+  },
+  button: {
+    backgroundColor: Colors.WHITE,
+    padding: 15,
+    paddingHorizontal: 5,
+    borderRadius: 99,
+    marginTop: 30,
+  },
+});
---
+++ utils/kindeConfig.jsx
@@ -0,0 +1,8 @@
+import { KindeSDK } from "@kinde-oss/react-native-sdk-0-7x";
+
+export const client = new KindeSDK(
+  "https://akshay090703.kinde.com",
+  "exp://192.168.1.5:8081",
+  "7bb6b591ca2d49c79b23e8c3587364b0",
+  "xp://192.168.1.5:8081"
+);
